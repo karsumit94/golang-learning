0abc569661cc27330a7e2e0928023b621e4ea183
Sumit Kar <skar@broadsoft.com>
2018-02-10 20:06:15 +0530

    added scope

--- 01-basics/05-scope.go
+++ 01-basics/05-scope.go
@@ -3,13 +3,32 @@ package main
 import "fmt"
 
 var x = 42
-
+func increment() int {
+	x++
+	return x
+}
 func main() {
 	//var x = 42
+	increment()
+	increment()
 	fmt.Println(x)
 	foo()
+
+	{
+		y := "Hello, World !!"
+		fmt.Println(y)
+	}
+	// fmt.Println(y) // outside scope of y
+
+/*
+closure helps us limit the scope of variables used by multiple functions
+without closure, for two or more funcs to have access to the same variable,
+that variable would need to be package scope
+*/
 }
 
 func foo() {
 	fmt.Println(x)
 }
+
+

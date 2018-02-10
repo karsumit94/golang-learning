fb03b5c82c3a2225d721c644455e4f5fc1951e6a
Sumit Kar <skar@broadsoft.com>
2018-02-10 22:17:00 +0530

    happy to commit

--- 01-basics/02-02-if-else.go
+++ 01-basics/02-02-if-else.go
@@ -3,6 +3,35 @@ package main
 import "fmt"
 
 func main() {
+
+
+	if true {
+		fmt.Println("This ran")
+	}
+
+	if false {
+		fmt.Println("This did not run")
+	}
+	fmt.Println()
+	fmt.Println()
+	
+	if !true {
+		fmt.Println("This did not run")
+	}
+
+	if !false {
+		fmt.Println("This ran")
+	}
+	fmt.Println()
+	fmt.Println()
+
+    b := true
+	if food := "Chocolate"; b {
+		fmt.Println(food)
+	}
+	fmt.Println()
+	fmt.Println()
+
 	x := 13 % 3
 	fmt.Println(x)
 	if x == 1 {
@@ -12,4 +41,5 @@ func main() {
 	}
 	fmt.Println()
 	fmt.Println()
+
 }

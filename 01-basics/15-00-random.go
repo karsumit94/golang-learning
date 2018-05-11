964e1c2ebf3895cb7200f93b4d67a17217f639df
Sumit Kar <skar@broadsoft.com>
2018-05-11 11:02:11 +0530

    Happy to commit

--- 01-basics/15-00-random.go
+++ 01-basics/15-00-random.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
+	//by default the seed for rand in golang is 1 
+	rand.Seed(time.Now().UnixNano())
 	x := rand.Intn(300000)
 	fmt.Println(x)
 }
